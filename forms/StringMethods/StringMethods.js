7022c88988eff249a1f085b720e932a93fd482f8
Lizzie <ekr74869@creighton.edu>
2019-09-18 16:33:42 -0500

    Changes

--- forms/StringMethods/StringMethods.js
+++ forms/StringMethods/StringMethods.js
@@ -1,1 +1,13 @@
-var quoteString = "Failure is simply the opportunity to begin again this time more intelligently."
+//2a
+var quoteString = "Failure is simply the opportunity to begin again this time more intelligently.";
+//2b
+var upperCaseString = quoteString.toUpperCase();
+//2c
+var authorString = " -Henry Ford";
+//2d
+var completeString = quoteString.concat(authorString);
+//2e
+var secondQuote = "Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work. - Stephen King"
+//2f
+var lowerCaseString = secondQuote.toLowerCase();
+console.log(lowerCaseString);
